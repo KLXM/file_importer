938be5c4e6269a19b72eb219d2f9e5ab7499691f
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-29 21:30:20 +0100

    Update AbstractProvider.php

--- lib/Provider/AbstractProvider.php
+++ lib/Provider/AbstractProvider.php
@@ -1,75 +1,115 @@
 <?php
-namespace Klxm\FileImporter;
+namespace Klxm\FileImporter\Provider;
 
-use Klxm\FileImporter\Provider\PixabayProvider;
+abstract class AbstractProvider implements ProviderInterface
+{
+    protected array $config = [];
+    protected string $cachePrefix = 'fileimporter.search.';
 
-// Nur im Backend ausführen
-if (\rex::isBackend() && \rex::getUser()) {
-
-    // Provider registrieren
-    if (!isset($this->providers)) {
-        $this->providers = [];
-    }
-    $this->providers['pixabay'] = new PixabayProvider();
-
-    // Assets nur auf der File-Importer-Seite einbinden
-    if (\rex_be_controller::getCurrentPage() == 'file_importer/main') {
-        \rex_view::addCssFile($this->getAssetsUrl('file_importer.css'));
-        \rex_view::addJsFile($this->getAssetsUrl('file_importer.js'));
+    public function __construct()
+    {
+        $this->loadConfig();
     }
 
-    // AJAX Handler für API Anfragen
-    if (\rex_request('file_importer_api', 'bool', false)) {
+    protected function loadConfig(): void
+    {
+        $this->config = \rex_config::get('file_importer', $this->getName()) ?? [];
+    }
+
+    protected function saveConfig(array $config): void
+    {
+        \rex_config::set('file_importer', $this->getName(), $config);
+    }
+
+    protected function getCacheKey(string $query, int $page, array $options = []): string
+    {
+        return $this->cachePrefix . $this->getName() . '.' . md5($query . $page . serialize($options));
+    }
+
+    protected function getFromCache(string $key)
+    {
+        $cacheFile = \rex_path::addonCache('file_importer', $key . '.cache');
+        if (!file_exists($cacheFile)) {
+            return false;
+        }
+
+        $cache = unserialize(file_get_contents($cacheFile));
+        if (!$cache || !isset($cache['expires']) || $cache['expires'] < time()) {
+            @unlink($cacheFile);
+            return false;
+        }
+
+        return $cache['data'];
+    }
+
+    protected function setCache(string $key, $data, int $expiration = 86400): void
+    {
+        $cacheFile = \rex_path::addonCache('file_importer', $key . '.cache');
+        $cache = [
+            'data' => $data,
+            'expires' => time() + $expiration
+        ];
+        
+        file_put_contents($cacheFile, serialize($cache));
+    }
+
+    protected function sanitizeFilename(string $filename): string
+    {
+        $filename = mb_convert_encoding($filename, 'UTF-8', 'auto');
+        $filename = \rex_string::normalize($filename);
+        $filename = preg_replace('/[^a-zA-Z0-9_-]/', '_', $filename);
+        return trim($filename, '_');
+    }
+
+    protected function downloadFile(string $url, string $filename): bool
+    {
         try {
-            $action = \rex_request('action', 'string');
-            $provider = \rex_request('provider', 'string');
-            $query = \rex_request('query', 'string', '');
-            $page = \rex_request('page', 'integer', 1);
+            $tmpFile = \rex_path::cache('file_importer_' . uniqid() . '_' . $filename);
             
-            if (!isset($this->providers[$provider])) {
-                throw new \rex_exception('Invalid provider');
+            $ch = curl_init($url);
+            $fp = fopen($tmpFile, 'wb');
+            
+            curl_setopt_array($ch, [
+                CURLOPT_FILE => $fp,
+                CURLOPT_HEADER => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_TIMEOUT => 60
+            ]);
+            
+            $success = curl_exec($ch);
+            
+            if (curl_errno($ch)) {
+                throw new \Exception(curl_error($ch));
             }
-
-            $providerInstance = $this->providers[$provider];
-
-            switch ($action) {
-                case 'search':
-                    $options = [
-                        'type' => \rex_request('type', 'string', 'all'),
-                        'category' => \rex_request('category', 'string', '')
-                    ];
-                    $results = $providerInstance->search($query, $page, $options);
-                    \rex_response::sendJson(['success' => true, 'data' => $results]);
-                    break;
-
-                case 'download':
-                    $url = \rex_request('url', 'string');
-                    $filename = \rex_request('filename', 'string');
-                    $result = $providerInstance->download($url, $filename);
-                    \rex_response::sendJson(['success' => $result]);
-                    break;
-
-                default:
-                    throw new \rex_exception('Invalid action');
+            
+            curl_close($ch);
+            fclose($fp);
+            
+            if ($success) {
+                $media = [
+                    'title' => pathinfo($filename, PATHINFO_FILENAME),
+                    'file' => [
+                        'name' => $filename,
+                        'path' => $tmpFile,
+                        'tmp_name' => $tmpFile
+                    ],
+                    'category_id' => \rex_post('category_id', 'int', 0)
+                ];
+                
+                $result = \rex_media_service::addMedia($media, true);
+                unlink($tmpFile);
+                
+                return $result;
             }
-
+            
+            return false;
+            
         } catch (\Exception $e) {
             \rex_logger::logException($e);
-            \rex_response::sendJson([
-                'success' => false, 
-                'error' => $e->getMessage()
-            ]);
+            if (file_exists($tmpFile)) {
+                unlink($tmpFile);
+            }
+            return false;
         }
-        exit;
     }
 }
-
-// Cache leeren wenn Provider-Einstellungen gespeichert werden
-\rex_extension::register('REX_FORM_SAVED', function($ep) {
-    if (strpos(\rex_be_controller::getCurrentPage(), 'file_importer') === 0) {
-        // Cache-Keys mit dem Prefix löschen
-        $cacheFolder = \rex_path::addonCache('file_importer');
-        if (is_dir($cacheFolder)) {
-            array_map('unlink', glob($cacheFolder . '/*.cache'));
-    }
-});
